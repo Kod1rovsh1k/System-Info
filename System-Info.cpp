7dce55324c58ba49e9a9315d9efb1cdbafe51139
? <kod1rovsh1kpr@gmail.com>
2024-12-02 21:49:29 +0200

    Добавьте файлы проекта.

---
+++ System-Info.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	system("cls");
+
+	std::string command = "systeminfo /FO LIST >> systeminfo.txt";
+
+	system("chcp 437 > nul");
+	system(command.c_str());
+	std::cout << "[OK] Information was successfully received" << std::endl;
+	system("pause");
+}
